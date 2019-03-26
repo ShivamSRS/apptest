c6248a2224e1dd804a832734e92629e94a2bf2f7
unknown <shivamraisharma@gmail.com>
2019-03-27 01:41:17 +0530

    Initial commit

---
+++ lib/dashboard.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatefulWidget {
+  @override
+  _DashboardState createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("NIMBUS"),
+        elevation: .1,
+        backgroundColor: Color.fromRGBO(49, 87, 110, 1.0),
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 2.0),
+        child: GridView.count(
+          crossAxisCount: 2,
+          padding: EdgeInsets.all(3.0),
+          children: <Widget>[
+            makeDashboardItem("The Magazine", Icons.book,0),
+            makeDashboardItem("About",Icons.album,1),
+            makeDashboardItem("Departmental Activities", Icons.threesixty,2),
+            makeDashboardItem("Archives", Icons.archive,3),
+            makeDashboardItem("Submit article", Icons.assignment,4),
+            makeDashboardItem("Deadlines", Icons.alarm,5)
+          ],
+        ),
+      ),
+    );
+  }
+
+  Card makeDashboardItem(String title, IconData icon,int i) {
+    return Card(
+        elevation: 1.0,
+        margin: new EdgeInsets.all(8.0),
+        child: Container(
+          decoration: BoxDecoration(color: Color.fromRGBO(220, 220, 220, 1.0)),
+          child: new InkWell(
+            onTap: () {
+              if(i==0){
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Carroussel()));
+                }},
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisSize: MainAxisSize.min,
+              verticalDirection: VerticalDirection.down,
+              children: <Widget>[
+                SizedBox(height: 50.0),
+                Center(
+                    child: Icon(
+                      icon,
+                      size: 40.0,
+                      color: Colors.black,
+                    )),
+                SizedBox(height: 20.0),
+                new Center(
+                  child: new Text(title,
+                      style:
+                      new TextStyle(fontSize: 18.0, color: Colors.black)),
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+}
+
+
+class Carroussel extends StatefulWidget {
+  @override
+  _CarrousselState createState() => new _CarrousselState();
+}
+
+class _CarrousselState extends State<Carroussel> {
+  PageController controller;
+  int currentpage = 0;
+
+  @override
+  initState() {
+    super.initState();
+    controller = new PageController(
+      initialPage: currentpage,
+      keepPage: false,
+      viewportFraction: 0.5,
+    );
+  }
+
+  @override
+  dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Center(
+        child: new Container(
+          child: new PageView.builder(
+              onPageChanged: (value) {
+                setState(() {
+                  currentpage = value;
+                });
+              },
+              controller: controller,
+              itemBuilder: (context, index) => builder(index)),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text("Go back"),
+        onPressed:(){Navigator.pop(context);},
+      ),
+    );
+  }
+
+  builder(int index) {
+    return new AnimatedBuilder(
+      animation: controller,
+      builder: (context, child) {
+        double value = 1.0;
+        if (controller.position.haveDimensions) {
+          value = controller.page - index;
+          value = (1 - (value.abs() * .5)).clamp(0.0, 1.0);
+        }
+
+        return new Center(
+          child: new SizedBox(
+            height: Curves.easeOut.transform(value) * 300,
+            width: Curves.easeOut.transform(value) * 250,
+            child: child,
+          ),
+        );
+      },
+      child: new Container(
+        margin: const EdgeInsets.all(8.0),
+        color: index % 2 == 0 ? Colors.blue : Colors.red,
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'dashboard.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      // home: new Dashboard(title: 'Flutter Demo Home Page'),
+      home: Dashboard(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
